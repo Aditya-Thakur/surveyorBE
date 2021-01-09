c2a7c2235d8584ead3ec0fd0ed414018147ed12f
Aditya <adityathakur532@gmail.com>
2021-01-09 20:22:25 +0530

    survey functions added

---
+++ controllers/surveyController.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const survey = mongoose.model('Survey');
+var router = express.Router();
+
+router.post('/fetchMySurveys', (req,res) => {
+    user.findOne( { createdBy: req.body.createdBy }, function(err, docs) {
+        if(!err) {
+            res.json(docs);
+        } else {
+            console.log('Error in fetching surveys: ' + err);
+        }
+    }); 
+});
+
+router.post('/changeStatus', (req,res) => {
+    survey.findOne({ id : req.body.id }, function(err, docs) {
+        if(!err){
+        docs.active = req.body.active;
+        docs.save(function(err) {
+            if(!err){
+                var response = {
+                    status: 100,
+                    message: "Survey status changed successfully."
+                }
+                res.json(response); 
+            }else {
+            console.log('Error in updating survey status: ' + err);
+            var response = {
+                status: 201,
+                message: "Error in updating survey status right now. Please contact administrator."
+            }
+            res.json(response); 
+        }
+        });
+        
+    } else {
+            console.log('Error in retrieving survey: ' + err);
+            var response = {
+                status: 202,
+                message: "Looks like this survey doesn't exist anymore. Please contact administrator."
+            }
+            res.json(response); 
+        }
+    });
+});
+
+router.post('/createSurvey', (req,res) => {
+    addSurvey(req,res);
+});
+
+function addSurvey(req,res) {
+    var newSurvey = new survey(); 
+    newSurvey.createdBy = req.body.createdBy;
+    newSurvey.createdOn = req.body.createdOn;
+    newSurvey.validTill = req.body.validTill;
+    newSurvey.active = req.body.active;
+    newSurvey.question = req.body.question;
+    newSurvey.save((err, docs) => {
+        if(!err){
+            var response = {
+                status: 100,
+                message: "Survey Created",
+                surveyCode: docs.id
+            }
+            res.json(response);
+        } else {
+            console.log('Error during Survey record insertion: ' + err);
+        }
+    });
+}
