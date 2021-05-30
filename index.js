c9d1520755efafe5fb861cff7dc38a6c442cbea9
Aditya <adityathakur532@gmail.com>
2021-05-31 03:36:25 +0530

    cors

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.listen(port, () => {
     console.log('Express server started at port: 3000');
 });
 app.use(cors());
+app.use( function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://surveyor-ui.vercel.app/');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+});
 app.use('/user', userController); 
 app.use('/survey', surveyController);
 app.use('/response', responseController); 
