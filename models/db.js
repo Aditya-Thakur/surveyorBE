cf033ab7c9c354397220b47e2e6ef98aeae36d9e
Aditya <adityathakur532@gmail.com>
2021-05-31 02:28:22 +0530

    vercel config

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/surveyDB', {useNewUrlParser: true}, (err) => {
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true}, (err) => {
     if(!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB Connection : '+ err)}
 });
