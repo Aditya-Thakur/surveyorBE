e9d2336a3d090d80dac3a61b5177946482e78cf7
Aditya <adityathakur532@gmail.com>
2021-05-31 03:40:26 +0530

    cors all

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.listen(port, () => {
 });
 app.use(cors());
 app.use( function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://surveyor-ui.vercel.app/');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     next();
